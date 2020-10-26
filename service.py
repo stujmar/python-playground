b1504fcd0acac0e190e9a19338094a702784432f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-10-25 21:35:21 -0500

    made flask method take in a name variable from the url path

--- service.py
+++ service.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/<name>')
+def hello(name):
+    return "Hello {}!".format(name)
