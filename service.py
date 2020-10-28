997a2a0c623e644a7caa60dc5bff084dcad21ab9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-10-27 20:44:44 -0500

    getting up to date

--- service.py
+++ service.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/<name>')
+def hello(name):
+    return "Hello {}!".format(name)
 
 def home():
     return "Welcome!"
     
-def hello(name):
-    return "Hello {}!".format(name)
