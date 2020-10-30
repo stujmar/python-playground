714ba876c896d9fe325698d606934559597f390e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-10-30 15:25:00 -0500

    moved fetching and printing words to two separate functions/methods

--- words.py
+++ words.py
@@ -8,7 +8,9 @@ def fetch():
         for word in line_words:
             story_words.append(word)
     story.close()
+    return story_words
 
+def print_words(story_words):
     for word in story_words:
         print(word)
 
