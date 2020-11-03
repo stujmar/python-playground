cead3e680ebe1cf2b5af05fb4be5c520ebbed906
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-11-02 23:16:18 -0600

    added a try catch block to the convert function

--- loop.py
+++ loop.py
@@ -11,14 +11,19 @@ teams = {
     "ten": "10",
 }
 
-c = 10
-while c != 0:
-    print("Go Team " + teams[c] + "!")
-    c -= 1
+# c = 10
+# while c != 0:
+#     print("Go Team " + teams[c] + "!")
+#     c -= 1
 
 def convert(input):
-    number = ''
-    for item in input:
-        number += teams[item]
-    x = int(number)
+    try:
+        number = ''
+        for item in input:
+            number += teams[item]
+        x = int(number)
+        print("good")
+    except KeyError:
+        print("bad")
+        x = -1
     return x
