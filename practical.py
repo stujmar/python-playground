606cb1754c882a2c8bbed9dacf70da2a31b82484
Stuart Marsh <stujmar@gmail.com>
2021-05-08 18:06:21 -0500

    [sm] cleaned up inputs got all the data type float, and made tax entered as a percentage

--- practical.py
+++ practical.py
@@ -3,8 +3,9 @@ width = 20
 area = length * width
 print (area)
 
-amount = 100
-tax = .06
-total = amount + (amount * tax)
+
+amount = float(input("What is your amount? "))
+tax = float(input("What is your tax percentage? "))
+total = amount + (amount * tax/100)
 print (f'the amount plus tax is {total}')
-print ('the amount plus tax is ' + str(total))
+# print ('the amount plus tax is ' + str(total))
