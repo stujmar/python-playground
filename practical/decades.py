942bc45b1a3ee99974410dda688f3279357c5769
Stuart Marsh <stujmar@gmail.com>
2021-05-09 21:18:58 -0500

    [sm] added some addtional text in a conditional

--- practical/decades.py
+++ practical/decades.py
@@ -2,3 +2,7 @@ age = float(input("How old are you?\n"))
 print (f'You are {age}')
 print (f'You are {int(age//10)} decades old ...')
 print (f'and an additional {int(age%10)} years :(\n')
+if age > 50:
+    print ("Wow you are old!")
+else:
+    print ("Wow you are young!")
