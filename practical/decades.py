ab4cb568970529c0457c74d4f83130a092c137a8
Stuart Marsh <stujmar@gmail.com>
2021-05-09 21:16:40 -0500

    [sm] finished up decades program

--- practical/decades.py
+++ practical/decades.py
@@ -1,3 +1,4 @@
 age = float(input("How old are you?\n"))
 print (f'You are {age}')
-print (f'You are {age/10} decades old\n')
+print (f'You are {int(age//10)} decades old ...')
+print (f'and an additional {int(age%10)} years :(\n')
