47bdd90c0537c1f2e81abf2f06f0f1e364c35e37
Stuart Marsh <stujmar@gmail.com>
2021-05-09 22:16:56 -0500

    [sm] added not keyword to check if it is raining

--- practical/temperature.py
+++ practical/temperature.py
@@ -1,8 +1,11 @@
 temp = input("what is the temperature?\n")
+forcast = "sun"
 
 if int(temp) > 90:
     print ("It sure is hot out.")
-elif int(temp) < 32:
+elif int(temp) < 32 and forcast == "rain":
+    print("Brrr. Watch out for freezing rain.")
+elif int(temp) < 32 and not forcast == "rain":
     print("Brrr.")
 else:
     print("Have fun outside.")
