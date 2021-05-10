7788b2f1e41285022608e0a729ee133c73e6e5f7
Stuart Marsh <stujmar@gmail.com>
2021-05-09 22:02:23 -0500

    [sm] tested an if an else and an elsif

--- practical/temperature.py
+++ practical/temperature.py
@@ -1,1 +1,9 @@
-print ('it sure is hot out')
+temp = input("what is the temperature?\n")
+
+if int(temp) > 90:
+    print ("It sure is hot out.")
+elif int(temp) < 32:
+    print("Brrr.")
+else:
+    print("Have fun outside.")
+
