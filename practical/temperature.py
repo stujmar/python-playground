d410eda10108fc7a42e050336ca2ddaf694b2bd7
Stuart Marsh <stujmar@gmail.com>
2021-05-09 22:19:57 -0500

    [sm] refactored conditionals to be cleaner with an is raining boolean

--- practical/temperature.py
+++ practical/temperature.py
@@ -1,11 +1,11 @@
 temp = input("what is the temperature?\n")
-forcast = "sun"
+is_raining = True
 
 if int(temp) > 90:
     print ("It sure is hot out.")
-elif int(temp) < 32 and forcast == "rain":
+elif int(temp) < 32 and is_raining:
     print("Brrr. Watch out for freezing rain.")
-elif int(temp) < 32 and not forcast == "rain":
+elif int(temp) < 32 and not is_raining:
     print("Brrr.")
 else:
     print("Have fun outside.")
