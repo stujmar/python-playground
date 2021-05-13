8404245a127f511cb9a72ef6093222493078bc0c
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-12 22:12:22 -0500

    [sm] got a nicely formated list of all the people in space

--- practical/json.py
+++ practical/json.py
@@ -3,4 +3,8 @@ import requests
 # api.open-nofity.org/astros.json
 response = requests.get('http://api.open-notify.org/astros.json')
 json = response.json()
-print(json)
+# print(json)
+
+print('The people currently in space are:')
+for person in json['people']:
+    print(person['name'], "in the", person['craft'])
