0a4f9f0baca0ee90e2e13a69baf7ccb0b3ddae2f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-01-06 22:31:44 -0600

    [sm] checked who all is in space I guess: :

---
+++ space.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    import requests
+    response = requests.get('http://api.open-notify.org/astros.json')
+    json = response.json()
+    print('The people currently in space are:')
+    for person in json['people']:
+        print(person['name'], "in the", person['craft'])
