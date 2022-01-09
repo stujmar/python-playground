2b7392a663e676897309c4283b491a41598155d4
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-01-08 23:14:24 -0600

    [sm] started to build out database module with functions

--- database.py
+++ database.py
@@ -1,5 +1,10 @@
 import sqlalchemy
 
+def load_data():
+  print('Loading data...')
+
 if __name__ == "__main__":
-  print("welcome to the database")
+  print("Welcome to the database.")
   print("sqlalchemy v:", sqlalchemy.__version__)
+  load_data()
+
