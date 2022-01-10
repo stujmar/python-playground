1bc13af34225b3bd53ba4f3f932804d2bf89faf3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-01-09 21:24:01 -0600

    [sm] created engine in python memory and a function to start and print it

--- database.py
+++ database.py
@@ -1,10 +1,18 @@
 import sqlalchemy
+from sqlalchemy import create_engine
 
 def load_data():
   print('Loading data...')
+
+def start_engine():
+  print('Starting engine...')
+  engine = create_engine('sqlite:///:memory:')
+  return engine
 
 if __name__ == "__main__":
   print("Welcome to the database.")
   print("sqlalchemy v:", sqlalchemy.__version__)
   load_data()
+  engine = start_engine()
+  print(engine)
 
