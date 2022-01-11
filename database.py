b7565b0b02caf367a701462c3af5421f1a46f223
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-01-10 22:47:33 -0600

    [sm] reworking through sqlalchemy tutorial

--- database.py
+++ database.py
@@ -1,5 +1,19 @@
 import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Cookie(Base):
+    __tablename__ = 'cookies'
+
+    cookie_id = sqlalchemy.Column(sqlalchemy.String(20), primary_key=True)
+    cookie_name = sqlalchemy.Column(sqlalchemy.String(20), nullable=False)
+    cookie_recipe_url = sqlalchemy.Column(sqlalchemy.String(255))
+    cookie_sku = sqlalchemy.Column(sqlalchemy.String(20))
+    quantity = sqlalchemy.Column(sqlalchemy.Integer)
+    unit_cost = sqlalchemy.Column(sqlalchemy.Numeric(12, 2))
 
 def load_data():
   print('Loading data...')
@@ -7,7 +21,17 @@ def load_data():
 def start_engine():
   print('Starting engine...')
   engine = create_engine('sqlite:///:memory:')
-  return engine
+  Session = sessionmaker(bind=engine)
+  session = Session()
+
+def add_cookie():
+  print('Adding cookie...')
+  cc_cookie = Cookie(
+    cookie_id='12345',
+    cookie_name='chocolate chip',
+    cookie_recipe_url='http://some.aweso.me/cookie/recipe.html',
+    cookie_sku='CC01',
+    quantity=12, unit_cost=0.50)
 
 if __name__ == "__main__":
   print("Welcome to the database.")
