24715bf89eba6d3fa83c4f39845735c68ba9a183
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-01-14 21:31:09 -0600

    [ms] adding a selecting tables from the db but getting some errors on table names

--- database.py
+++ database.py
@@ -29,6 +29,9 @@ class Database:
     self.session.add(cookie)
     self.session.commit()
 
+  def select_cookie(self, cookie_id):
+    return self.session.query(Cookie).filter_by(cookie_id=cookie_id).one()
+
   def get_cookie(self, cookie_id):
     return self.session.query(Cookie).filter_by(cookie_id=cookie_id).one_or_none()
 
@@ -62,7 +65,10 @@ if __name__ == "__main__":
   my_db.say_hello()
 
   my_cookie = get_cookie()
-  print(my_cookie.cookie_name)
+  my_db.add_cookie(my_cookie)
+  is_my_cookie = my_db.select_cookie(my_cookie.cookie_id)
+
+  print(is_my_cookie)
 
   # load_data()
   # engine = start_engine()
